d363e2a3efdff40fa989d8d1361f393199bbe765
sphathimandla-makhanya <makhanyasm11@gmail.com>
2024-02-28 09:42:41 +0200

    Added content

--- controller/products.js
+++ controller/products.js
@@ -3,64 +3,115 @@ import { getProducts, getSingle, postProduct,deleteProduct,updateProduct,getUser
 
 export default{
     getAll: async (req,res)=>{
-        res.send(await getProducts())
+        try{
+            res.send(await getProducts())
+        }catch(error){
+            console.log(error)
+            res.status(500).json({error:'Internal server error'})
+        }
     },
     getSingle: async (req,res)=>{
-        res.send(await getSingle(+req.params.prodID))
+        try{
+            res.send(await getSingle(+req.params.prodID))
+        }catch(error){
+            console.log(error)
+            res.status(500).json({error:'Internal server error'})
+        }
     }, 
     addProduct: async (req,res)=>{
-        const {prodName, quantity, amount, category, prodUrl} = req.body
-        const post =  await postProduct(prodName, quantity, amount, category, prodUrl)
-        res.send(await getProducts())
+        try{
+            const {prodName, quantity, amount, category, prodUrl} = req.body
+            const post =  await postProduct(prodName, quantity, amount, category, prodUrl)
+            res.send(await getProducts())
+        }catch(error){
+            console.log(error)
+            res.status(500).json({error:'Internal server error'})
+        }
     },
     deleteItem: async (req,res)=>{
-        await deleteProduct(req.params.prodID)
-        res.json(await getProducts())
+        try{
+            await deleteProduct(req.params.prodID)
+            res.json(await getProducts())
+        }catch(error){
+            console.log(error)
+            res.status(500).json({error:'Internal server error'})
+        }
     }, 
     editProduct: async (req,res)=>{
-        const [product] = await getSingle(+req.params.prodID)
-        let {prodName, quantity, amount, category, prodUrl} = req.body
-        prodName ? prodName=prodName : {prodName}=product
-        quantity ? quantity=quantity : {quantity}=product
-        amount ? amount=amount : {amount}=product
-        category ? category=category : {category}= product
-        prodUrl ? prodUrl=prodUrl : {prodUrl}=product
-        const edit = await updateProduct(prodName, quantity, amount, category, prodUrl,+req.params.prodID)
-        res.json(await getProducts())
-    },
+        try{
+            const [product] = await getSingle(+req.params.prodID)
+            let {prodName, quantity, amount, category, prodUrl} = req.body
+            prodName ? prodName=prodName : {prodName}=product
+            quantity ? quantity=quantity : {quantity}=product
+            amount ? amount=amount : {amount}=product
+            category ? category=category : {category}= product
+            prodUrl ? prodUrl=prodUrl : {prodUrl}=product
+            const edit = await updateProduct(prodName, quantity, amount, category, prodUrl,+req.params.prodID)
+            res.json(await getProducts())
+        }catch(error){
+            console.log(error)
+                res.status(500).json({error:'Internal server error'})
+        }
+        },
     users: async (req,res)=>{
-        res.send(await getUsers())
+        try{
+            res.send(await getUsers())
+        }catch(error){
+            console.log(error)
+            res.status(500).json({error:'Internal server error'})
+        }
     },
     getUser: async (req,res)=>{
-        res.send(await getUser(+req.params.userID))
+        try{
+            res.send(await getUser(+req.params.userID))
+        }catch(error){
+            console.log(error)
+            res.status(500).json({error:'Internal server error'})
+        }
     },
     newUser: async(req,res)=>{
-        const {firstName, lastName, userAge, gender, userRole, emailAdd, userPass, userProfile}=req.body
-        bcrypt.hash(userPass,10,async(err,hash)=>{
-            if(err) throw err
-            await addUser(firstName, lastName, userAge, gender, userRole,emailAdd,hash, userProfile)
-            res.send({
-                msg: "You have created an account"
-            })
-    })
+        try{
+            const {firstName, lastName, userAge, gender, userRole, emailAdd, userPass, userProfile}=req.body
+            bcrypt.hash(userPass,10,async(err,hash)=>{
+                if(err) throw err
+                await addUser(firstName, lastName, userAge, gender, userRole,emailAdd,hash, userProfile)
+                res.send({
+                    msg: "You have created an account"
+                })
+                 })
+        }catch(error){
+            console.log(error)
+            res.status(500).json({error:'Internal server error'})
+        }
     },
     editUser: async (req,res)=>{
-        const [user] = await getUser(+req.params.userID)
-        let {firstName, lastName, userAge, gender, userRole,emailAdd,userPass, userProfile} = req.body
-        firstName ? firstName=firstName : {firstName}=user
-        lastName ? lastName=lastName : {lastName}=user
-        userAge ? userAge=userAge : {userAge}=user
-        gender ? gender=gender : {gender}= user
-        userRole ? userRole=userRole : {userRole}=user
-        emailAdd ? emailAdd=emailAdd : {emailAdd}=user
-        userPass ? userPass=userPass : {userPass}=user
-        userProfile ? userProfile=userProfile : {userProfile}=user
-        const edit = await updateUser(firstName, lastName, userAge, gender, userRole,emailAdd,userPass, userProfile,+req.params.userID)
-        res.json(await getUsers())
-    },
+        try{
+            const [user] = await getUser(+req.params.userID)
+            let {firstName, lastName, userAge, gender, userRole,emailAdd,userPass, userProfile} = req.body
+            firstName ? firstName=firstName : {firstName}=user
+            lastName ? lastName=lastName : {lastName}=user
+            userAge ? userAge=userAge : {userAge}=user
+            gender ? gender=gender : {gender}= user
+            userRole ? userRole=userRole : {userRole}=user
+            emailAdd ? emailAdd=emailAdd : {emailAdd}=user
+            userPass ? userPass=userPass : {userPass}=user
+            userProfile ? userProfile=userProfile : {userProfile}=user
+            const edit = await updateUser(firstName, lastName, userAge, gender, userRole,emailAdd,userPass, userProfile,+req.params.userID)
+            res.json(await getUsers())
+        }catch(error){
+            console.log(error)
+            res.status(500).json({error:'Internal server error'})
+            }
+        }
+    ,
     removeUser: async (req,res)=>{
-        await deleteUser(req.params.userID)
-        res.json(await getUser())
+        try{
+            await deleteUser(req.params.userID)
+            res.json(await getUser())
+        }catch(error){
+            console.log(error)
+            res.status(500).json({error:'Internal server error'})
+        }
     }
 
 }
